3ae0c8e1e2a986855e531ecb8d3958f91e5b3016
mlada <mlada.pechorina@gmail.com>
2024-10-02 13:24:11 +0700

    фикс

--- start.py
+++ start.py
@@ -2,20 +2,50 @@ from common import *
 from key_generator import *
 from crypt_module import *
 
+#Алгоритм шифрования Фейстеля
+#Преобразовать простой текст в ASCII, а затем в 8-битный двоичный формат. 
 initialKey = readFile("key.txt")
 print(initialKey)
-binaryKey = string_to_binary(initialKey)
-print(binaryKey)
-KeyGenerator1 = KeyGenerator(binaryKey)
-print(KeyGenerator1.keys);
-  
+
+# Converting the plain text to
+# ASCII
+Key_Ascii = [ord(x) for x in initialKey]
+print(Key_Ascii)
+
+# Converting the ASCII to 
+# 8-bit binary format
+Key_Bin = [format(y,'08b') for y in Key_Ascii]
+Key_Bin = "".join(Key_Bin)
+
+#Преобразовать простой текст в ASCII, а затем в 8-битный двоичный формат.   
 initialMessage = readFile('message.txt')
 print(initialMessage)
-binaryMessage = string_to_binary(initialMessage)
-print(binaryMessage)
+# Converting the plain text to
+# ASCII
+Message_Ascii = [ord(x) for x in initialMessage]
+print(Message_Ascii)
+
+# Converting the ASCII to 
+# 8-bit binary format
+Message_Bin = [format(y,'08b') for y in Message_Ascii]
+Message_Bin = "".join(Message_Bin)
+
+#sub_key = input("Write subkey variant: ")
+
+
+KeyGenerator1 = KeyGenerator(Key_Bin)
+print(KeyGenerator1.keys);
+
 Crypt1 = Crypt(KeyGenerator1)
-encrypted_msg = Crypt1.encrypt(binaryMessage)
+encrypted_msg = Crypt1.encrypt(Message_Bin)
 print(encrypted_msg)
 decrypted_msg = Crypt1.decrypt(encrypted_msg)
 print(decrypted_msg)
 print(binary_to_string(decrypted_msg))
+
+ 
+
+ 
+#Разделите двоичную строку обычного текста на две половины: левую половину (L1) и правую половину (R1) 
+ 
+#Сгенерируйте случайные двоичные ключи (K1 и K2) длиной, равной половине длины открытого текста для двух раундов. 
